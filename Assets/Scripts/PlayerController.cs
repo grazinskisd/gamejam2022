37edd7942a6e37eb84a0dfd30a982d8348a4353c
Dalius Grazinskis <grazinskisd@gmail.com>
2022-01-28 19:56:13 +0200

    Add some test scripts

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 1;
+
+    private void Update()
+    {
+        var input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        transform.position += input * Time.deltaTime * moveSpeed;
+    }
+}
---
+++ Assets/Scripts/ShootBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+public class ShootBehaviour : MonoBehaviour
+{
+    public Rigidbody2D bulletPrototype;
+    public float shootSpeed;
+    public KeyCode fireKey;
+
+    private float _lastShootTime;
+
+    private void Awake()
+    {
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(fireKey))
+        {
+            if (Time.time - _lastShootTime >= (1 / shootSpeed))
+            {
+            }
+        }
+    }
+}
