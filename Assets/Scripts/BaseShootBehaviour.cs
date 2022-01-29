6fffaab6e86332aee29b06952e2bddc8b3e65f0b
Dalius Grazinskis <grazinskisd@gmail.com>
2022-01-29 13:03:11 +0200

    Implement enemy shoot behaviour

---
+++ Assets/Scripts/BaseShootBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public abstract class BaseShootBehaviour : MonoBehaviour
+{
+    public Transform shootOrigin;
+    public string bulletLayerName;
+
+    public ShootingBehaviourSettings[] settings;
+
+    private ShootingBehaviourSettings[] _settingsCopy;
+    private Transform _bulletHolder;
+    private int _bulletLayer;
+
+    protected abstract float Angle { get; }
+
+    private void Awake()
+    {
+        _settingsCopy = new ShootingBehaviourSettings[settings.Length];
+        for (int i = 0; i < settings.Length; i++)
+        {
+            _settingsCopy[i] = Instantiate(settings[i]);
+        }
+
+        _bulletHolder = GameObject.Find("BulletHolder").transform;
+        _bulletLayer = LayerMask.NameToLayer(bulletLayerName);
+    }
+
+    private void Update()
+    {
+        Shoot();
+    }
+
+    protected virtual void Shoot()
+    {
+        FireFromAllSettings();
+    }
+
+    protected void FireFromAllSettings()
+    {
+        for (int i = 0; i < _settingsCopy.Length; i++)
+        {
+            _settingsCopy[i].Fire(shootOrigin.position, Angle, _bulletHolder, _bulletLayer);
+        }
+    }
+}
--- Assets/Scripts/ShootBehaviour.cs
+++ Assets/Scripts/ShootBehaviour.cs
@@ -1,28 +1,15 @@
 ﻿using UnityEngine;
-public class ShootBehaviour : MonoBehaviour
+public class ShootBehaviour : BaseShootBehaviour
 {
     public KeyCode fireKey;
-    public Transform shootOrigin;
-    public string bulletLayerName;
 
-    public ShootingBehaviourSettings settings;
+    protected override float Angle => -90;
 
-    private ShootingBehaviourSettings _settingsCopy;
-    private Transform _bulletHolder;
-    private int _bulletLayer;
-
-    private void Awake()
-    {
-        _settingsCopy = Instantiate(settings);
-        _bulletHolder = new GameObject("Bullet holder").transform;
-        _bulletLayer = LayerMask.NameToLayer(bulletLayerName);
-    }
-
-    private void Update()
+    protected override void Shoot()
     {
         if (Input.GetKey(fireKey))
         {
-            _settingsCopy.Fire(shootOrigin.position, -90, _bulletHolder, _bulletLayer);
+            base.Shoot();
         }
     }
 }
