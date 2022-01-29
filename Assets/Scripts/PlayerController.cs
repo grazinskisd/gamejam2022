0ec4a6b65209a527c49e17ac6329a1d47c147d32
Dalius Grazinskis <grazinskisd@gmail.com>
2022-01-29 18:37:53 +0200

    Add flies

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine.Events;
 public class PlayerController : MonoBehaviour
 {
     private const string RuneTag = "Rune";
+    private const string StopMoveTag = "MoveStop";
     public float moveSpeed = 1;
     public LayerMask enemyProjectileMask;
 
@@ -12,21 +13,26 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D _ridigbody;
     private bool _hasMovementStarted;
     private GameObject _currentRune;
+    private bool _canMove;
 
     private void Awake()
     {
+        _canMove = true;
         _ridigbody = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
-        var input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        if(!_hasMovementStarted && input.sqrMagnitude > 0)
+        if (_canMove)
         {
-            OnMoveStart?.Invoke();
-            _hasMovementStarted = true;
+            var input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            if (!_hasMovementStarted && input.sqrMagnitude > 0)
+            {
+                OnMoveStart?.Invoke();
+                _hasMovementStarted = true;
+            }
+            _ridigbody.MovePosition(_ridigbody.position + (input * Time.fixedDeltaTime * moveSpeed));
         }
-        _ridigbody.MovePosition(_ridigbody.position + (input * Time.fixedDeltaTime * moveSpeed));
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -41,6 +47,19 @@ public class PlayerController : MonoBehaviour
             _currentRune = collision.gameObject;
             _currentRune.gameObject.SetActive(false);
             _currentRune.GetComponent<Pickup>().IssuePickupEvent();
+        }
+
+        if (collision.gameObject.CompareTag(StopMoveTag))
+        {
+            _canMove = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(StopMoveTag))
+        {
+            _canMove = true;
         }
     }
 }
