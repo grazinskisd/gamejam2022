5f85d3b1d1b63db811366bc4299dd92c4ebd1f95
Dalius Grazinskis <grazinskisd@gmail.com>
2022-01-29 12:27:31 +0200

    Implement simple shooting settings

--- Assets/Scripts/ShootBehaviour.cs
+++ Assets/Scripts/ShootBehaviour.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
 public class ShootBehaviour : MonoBehaviour
 {
-    public Bullet bulletPrototype;
     public float shootSpeed;
     public KeyCode fireKey;
-    public Vector3 projectileVelocity;
     public Transform shootOrigin;
+
+    public ShootingBehaviourSettings settings;
 
     private float _lastShootTime;
     private Transform _bulletHolder;
@@ -21,9 +21,8 @@ public class ShootBehaviour : MonoBehaviour
         {
             if (Time.time - _lastShootTime >= (1 / shootSpeed))
             {
-                var bullet = Instantiate(bulletPrototype, _bulletHolder);
-                bullet.transform.position = shootOrigin.position;
-                bullet.velocity = projectileVelocity;
+                _lastShootTime = Time.time;
+                settings.Fire(shootOrigin.position, -90, _bulletHolder);
             }
         }
     }
---
+++ Assets/Scripts/ShootingBehaviourSettings.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "GameJam2022/ShootingBehaviourSettins")]
+public class ShootingBehaviourSettings : ScriptableObject
+{
+    public Bullet bulletPrototype;
+    public float projectileSpeed;
+
+    public Bullet Fire(Vector3 originPosition, float direction, Transform parent)
+    {
+        var bullet = Instantiate(bulletPrototype, parent);
+        bullet.transform.position = originPosition;
+
+        var rotation = Quaternion.AngleAxis(direction, Vector3.forward);
+        bullet.velocity = rotation * new Vector3(0, projectileSpeed, 0);
+
+        return bullet;
+    }
+}
