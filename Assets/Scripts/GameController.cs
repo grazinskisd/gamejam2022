efdeae4f6b727d247aecd9d78bfd00bed6733716
Dalius Grazinskis <grazinskisd@gmail.com>
2022-01-30 12:07:06 +0200

    Remove debug

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -118,7 +118,6 @@ public class GameController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Debug.Log(_state);
         switch (_state)
         {
             case GameState.Init:
