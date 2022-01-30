0cc6587491b01036eeabff2a55bdb6016fdb578a
Dalius Grazinskis <grazinskisd@gmail.com>
2022-01-30 10:51:32 +0200

    Add all player attacks

---
+++ Assets/Scripts/ShootingSettings/TrippleShooting.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "GameJam2022/TrippleShooting")]
+public class TrippleShooting : ShootingBehaviourSettings
+{
+    public float angleOffset = 30;
+
+    protected override void CreateBullet(Vector3 originPosition, float direction, Transform parent, int layer)
+    {
+        for (int i = -1; i <= 1 ; i++)
+        {
+            var adjustedDirection = direction + i * angleOffset;
+
+            var bullet = Instantiate(bulletPrototype, parent);
+            bullet.transform.position = originPosition;
+
+            var rotation = Quaternion.AngleAxis(adjustedDirection, Vector3.forward);
+            bullet.velocity = rotation * new Vector3(0, projectileSpeed, 0);
+            bullet.gameObject.layer = layer;
+        }
+    }
+}
